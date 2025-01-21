167301cbb08e2d75f7d6f0cf9e6880ad0d010e1f
Anastasia Bykasova <nastybykasova@gmail.com>
2025-01-21 11:50:41 +0300

    delete client by id

--- src/main/java/com/example/ShopAPI/services/ClientService.java
+++ src/main/java/com/example/ShopAPI/services/ClientService.java
@@ -1,14 +1,13 @@
 package com.example.ShopAPI.services;
 
-import com.example.ShopAPI.DTOs.ClientDto;
 import com.example.ShopAPI.DTOs.ClientRequestDto;
 import com.example.ShopAPI.DTOs.ClientResponseDto;
-import com.example.ShopAPI.configs.NotFoundException;
 import com.example.ShopAPI.mappers.ClientMapper;
 import com.example.ShopAPI.models.Address;
 import com.example.ShopAPI.models.Client;
 import com.example.ShopAPI.repositories.AddressRepository;
 import com.example.ShopAPI.repositories.ClientRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -34,65 +33,14 @@ public class ClientService {
         Address address = addressRepository.findById(clientRequestDTO.getAddressId())
                 .orElseThrow(() -> new IllegalArgumentException("Address not found with id: " + clientRequestDTO.getAddressId()));
 
-        client.setAddress(address); // Устанавливаем связанный объект Address
+        client.setAddress(address); // связанный объект Address
         Client savedClient = clientRepository.save(client);
 
         return clientMapper.clientToClientResponseDto(savedClient);
     }
+
+    public void deleteClient(UUID id) {
+        Client client = clientRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Client not found with id: " + id));
+        clientRepository.delete(client);
+    }
 }
-
-
-
-
-
-
-
-
-//import java.util.UUID;
-//
-//@Service
-//public class ClientService {
-//    private final ClientRepository clientRepository;
-//    private final ClientMapper clientMapper;
-//    private final AddressRepository addressRepository;
-//
-//    public ClientService(ClientRepository clientRepository, ClientMapper clientMapper, AddressRepository addressRepository) {
-//        this.clientRepository = clientRepository;
-//        this.clientMapper = clientMapper;
-//        this.addressRepository = addressRepository;
-//    }
-//
-//    // Добавление клиента (POST /clients):
-//    public ClientDto createClient(ClientDto clientDto) {
-//        UUID addressId = clientDto.getAddressId();
-//        Address address = addressRepository.findById(addressId)
-//                .orElseThrow(() -> new NotFoundException("Address not found with id: " + addressId));
-//        Client client = clientMapper.clientDtoToClient(clientDto);
-//        client.setAddressId(address);
-//        Client createdClient = clientRepository.save(client);
-//        return clientMapper.clientToClientDto(createdClient);
-//    }
-//
-//    // Обновление клиента (PUT /api/v1/clients/{id}):
-//    public ClientDto updateClient(UUID id, ClientDto clientDto) {
-//        Client existingClient = clientRepository.findById(id)
-//                .orElseThrow(() -> new NotFoundException("Client not found with id: " + id));
-//        Address address = addressRepository.findById(clientDto.getAddressId())
-//                .orElseThrow(() -> new NotFoundException("Address not found with id: " + clientDto.getAddressId()));
-//        Client client = clientMapper.clientDtoToClient(clientDto);
-//        client.setId(existingClient.getId());
-//        client.setAddressId(address);
-//        Client updatedClient = clientRepository.save(client);
-//        return clientMapper.clientToClientDto(updatedClient);
-//    }
-//
-//    // Получение клиента по id (GET /api/v1/clients/{id}):
-//    public ClientDto getClientById(UUID id) {
-//        Client client = clientRepository.findById(id)
-//                .orElseThrow(() -> new NotFoundException("Client not found with id: " + id));
-//        return clientMapper.clientToClientDto(client);
-//    }
-//
-//
-//
-//}
